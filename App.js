724761ce4e36f811d7c47a99c7f3b239ea9da14a
Fatymata <fatymata.gassama@gmail.com>
2020-09-30 00:23:32 +0200

    favorites & myPictures done + upload images

--- App.js
+++ App.js
@@ -1,21 +1,22 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import * as eva from '@eva-design/eva';
+import { StyleSheet, View, SafeAreaView } from "react-native";
+import { ApplicationProvider, Layout, Text } from '@ui-kitten/components';
+import Navigation from './components/Navigation'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.aaaajs to start working on your !!!app!!!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+const HomeScreen = () => (
+  <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+
+    <Text category='h1'>HOME</Text>
+  </Layout>
+);
+
+export default () => (
+  <ApplicationProvider {...eva} theme={eva.dark}>
+    <SafeAreaView style={{ flex: 1, marginTop: 30, marginBottom: 45 }} >
+      <Navigation></Navigation>
+    </SafeAreaView>
+  </ApplicationProvider>
+);
