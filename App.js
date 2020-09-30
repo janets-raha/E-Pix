2f6777c3c63591d6e8f74fff08572c6d99d6a51f
byynaryy <endeki@gmail.com>
2020-09-30 21:53:18 +0200

    add searchbar component

--- App.js
+++ App.js
@@ -5,18 +5,14 @@ import "react-native-gesture-handler";
 import React, { useState, useEffect } from "react";
 import { StyleSheet, View, Button, SafeAreaView } from "react-native";
 import Auth from "./components/Auth";
-
-const HomeScreen = () => (
-  <Layout style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-    <Text category="h1">HOME</Text>
-  </Layout>
-);
+import Searchbar from "./components/Searchbar";
 
 export default () => (
   <ApplicationProvider {...eva} theme={eva.light}>
     <SafeAreaView style={{ flex: 1, marginTop: 30, marginBottom: 45 }}>
       {/*  <Auth /> */}
-      <Navigation></Navigation>
+      <Searchbar></Searchbar>
     </SafeAreaView>
+
   </ApplicationProvider>
 );
